317f5c01dc087f9ccfb462a5c3d759cd63bd8fc2
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-23 09:27:56 -0400

    wip

--- api/auth/auth-model.js
+++ api/auth/auth-model.js
@@ -4,6 +4,13 @@ function getUserBy(filter) {
   return db('users').where({ filter }).first();
 }
 
+async function addUser(newUser) {
+  const user = await db('users').insert(newUser)
+    .then(([id]) => db('users').where({ id }).first());
+  return user;
+}
+
 module.exports = {
   getUserBy,
+  addUser,
 };
